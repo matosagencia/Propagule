dd84589f0722764794e0fa1587da07c0434c4a94
bruno de matos <54540129+matosagencia@users.noreply.github.com>
2023-06-11 00:02:49 -0300

    Propagule
    Propagule é um termo usado para sementes

--- propagule.php
+++ propagule.php
@@ -70,7 +70,7 @@
 <body>
     <div class="container">
         <div class="box">
-            <h2>Conversor de Palavras Chave para Números</h2>
+            <h2>Propagule - Suas sementes seguras</h2>
             <form method="post" action="" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="input">Digite as palavras semente da sua carteira Bitcoin (separadas por vírgula):</label>
